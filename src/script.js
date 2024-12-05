e6c35ed9dfa2ae3fd14aa39f2e8f272b1e38c5ab
Lill <lillcmoen@gmail.com>
2024-12-05 18:36:03 +0100

    Injected the forecast HTML from JavaScript

--- src/script.js
+++ src/script.js
@@ -52,7 +52,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-temperature">
+              <div class="weather-forecast-temperature-highest">
+                <strong>H:15°</strong>
+              </div>
+              <div class="weather-forecast-temperature-lowest">L:9°</div>
+            </div>
+          </div>
+          `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Barcelona");
+displayForecast();
